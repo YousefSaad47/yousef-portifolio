62635831202211027dc6ff918a5a20578183c07f
yousef <YoussefSaad299@gmail.com>
2024-10-21 16:53:51 +0300

    Add the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from './components/Hero';
 export default function Home() {
   return (
     <div className="h-full w-full">
-      <div className="flex flex-col gap-20 h-[850px]">
+      <div className="flex flex-col gap-20">
         <Hero />
       </div>
     </div>
