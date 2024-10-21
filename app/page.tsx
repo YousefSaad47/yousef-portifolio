88dcdac451c5ff7640d7541375dee8bd799c01ea
yousef <YoussefSaad299@gmail.com>
2024-10-21 12:06:18 +0300

    Finished the Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Hero from './components/Hero';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="h-full w-full">
+      <div className="flex flex-col gap-20 h-[850px]">
+        <Hero />
+      </div>
+    </div>
+  );
 }
